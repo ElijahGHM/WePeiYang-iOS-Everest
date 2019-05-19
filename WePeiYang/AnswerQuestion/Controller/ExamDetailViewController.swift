8336aca29479c6c2e58361c860501fb777a0dd89
mgh990806 <1075574784@qq.com>
2019-05-19 09:24:00 +0800

    更新了考试界面的代码

--- WePeiYang/AnswerQuestion/Controller/ExamDetailViewController.swift
+++ WePeiYang/AnswerQuestion/Controller/ExamDetailViewController.swift
@@ -8,10 +8,76 @@
 
 import UIKit
 class ExamDetailViewController: UIViewController {
-    
+    let myTableView = UITableView(frame: .zero, style: .plain)
+    let footButton = UIButton(frame: CGRect(x: 44, y: 0, width: Device.width, height: 64))
+    let tableViewCellTitle  = ["考试名称", "考试时间", "历史最高分", "通过状态", "有效成绩", "剩余考试次数"]
     override func viewDidLoad() {
         super.viewDidLoad()
         self.navigationItem.title = "考试详情"
         self.view.backgroundColor = .white
+        footButton.backgroundColor = .newBlue
+        footButton.setTitle("开始考试", for: .normal)
+        footButton.layer.cornerRadius = 16
+        footButton.addTarget(self, action: #selector(ExamDetailViewController.click), for: .touchUpInside)
+        loadTableView()
     }
+    func loadTableView() {
+        myTableView.frame = CGRect(x: 22, y: 0, width: Device.width - 44, height: 700)
+        myTableView.dataSource = self
+        myTableView.delegate = self
+//        myTableView.isUserInteractionEnabled = false
+        
+        view.addSubview(myTableView)
+    }
+    @objc func click(button: UIButton) {
+        let alert = UIAlertController(title: "提示", message: "你将开始考试", preferredStyle: .alert)
+        
+        let subButton = UIAlertAction(title: "确认", style: .default, handler: self.sub)
+        let cancelButton = UIAlertAction(title: "取消", style: .cancel, handler: nil)
+        alert.addAction(subButton)
+        alert.addAction(cancelButton)
+        self.present(alert, animated: true, completion: nil)
+    }
+    func sub(alert: UIAlertAction) {
+        print("开始考试")
+    }
+}
+extension UIColor {
+    static var newBlue: UIColor {
+        return UIColor(red: 62/255, green: 129/255, blue: 228/255, alpha: 1.0)
+    }
+}
+
+extension ExamDetailViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: false)
+    }
+}
+
+extension ExamDetailViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 6
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = TestViewTableViewCell()
+        cell.selectionStyle = UITableViewCell.SelectionStyle.none;
+        cell.titleLable.text = tableViewCellTitle[indexPath.row]
+        
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 84
+    }
+    
+    func tableView(_ tableView: UITableView, viewForFooterInSection section: Int) -> UIView? {
+        return footButton
+    }
+    func tableView(_ tableView: UITableView, heightForFooterInSection section: Int) -> CGFloat {
+        return 64
+    }
+
+    
+    
 }
---
+++ WePeiYang/AnswerQuestion/View/TestViewTableViewCell.swift
@@ -0,0 +1,34 @@
+//
+//  TestViewTableViewCell.swift
+//  Test1
+//
+//  Created by Elijah on 5/18/19.
+//  Copyright © 2019 Elijah. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class TestViewTableViewCell: UITableViewCell {
+    let titleLable = UILabel()
+    let Lable = UILabel()
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+    }
+    convenience init() {
+        self.init(style: .default, reuseIdentifier: "TestViewTableViewCell")
+        titleLable.frame = CGRect(x: 20, y: 20, width: 150, height: 20)
+        titleLable.textColor = .newBlue
+        titleLable.text = "这是标题"
+        contentView.addSubview(titleLable)
+        
+        Lable.frame = CGRect(x: 0, y: 60, width: 150, height: 20)
+        Lable.text = "等待接口"
+        contentView.addSubview(Lable)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
